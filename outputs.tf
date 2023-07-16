2144c035608e4dc0e8c48b63f3f8a5e97537aec1
harshanannapaneni <80609202+harshanannapaneni@users.noreply.github.com>
2023-07-16 15:21:19 -0400

    Blue deployment release version

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,39 @@ output "availability_zones" {
 output "windows_ami" {
   value = "${data.aws_ami.windows.id}"
 }
+
+# Output amazon linux ami
+output "amazon_linux_ami" {
+  value = "${data.aws_ami.amazon_linux.id}"
+}
+
+# Output vpc id
+output "vpc_id" {
+  value = "${module.vpc.vpc_id}"
+}
+
+# Output public subnets
+output "public_subnets" {
+  value = "${module.vpc.public_subnets}"
+}
+
+# Output private subnets
+output "private_subnets" {
+  value = "${module.vpc.private_subnets}"
+}
+
+# Output app security group id
+output "app_security_group_id" {
+  value = "${module.app_security_group.security_group_id}"
+}
+
+# Output lb security group id
+output "lb_security_group_id" {
+  value = "${module.lb_security_group.security_group_id}"
+}
+
+# Output lb security group name
+output "lb_security_group_name" {
+  value = "${module.lb_security_group.security_group_name}"
+}
+
